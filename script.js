b4b9fbca958a1d8031ba4105ad88414104ac1771
bugthug <avivyodfat@proton.me>
2023-04-26 10:42:47 +0300

    implement createGrid

---
+++ script.js
@@ -0,0 +1,20 @@
+createGrid(10);
+
+function createGrid(gridSize) {
+    const container = document.querySelector('.container');
+    for (let i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement('div')
+        gridRow.classList.add('grid-row')
+        for (let ii = 0; ii < gridSize; ii++) {
+            const gridRowCell = document.createElement('div')
+            gridRowCell.addEventListener('mouseover', function(){
+                this.style.backgroundColor = 'red';
+            })
+            gridRowCell.classList.add('grid-cell');
+            gridRow.appendChild(gridRowCell);
+        }
+        container.appendChild(gridRow);
+    }
+}
+
+
